6b1a7c832b43c0d896373d87b03c12e35f6e8f78
Ana <e4a.perez24@gmail.com>
2023-07-12 11:39:30 +0200

    div discover

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,9 +1,15 @@
 import Header from '../../components/header/Header';
 
+import './home.css';
+
 function Home() {
   return (
     <>
       <Header />
+      <section className='section-home'>
+        <h2>Discover your City</h2>
+        <p>Discover fun new cities below</p>
+      </section>
     </>
   );
 }
