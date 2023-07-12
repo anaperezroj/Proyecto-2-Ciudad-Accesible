cfb487b2587372b87a999a306790581358aeea20
Ana <e4a.perez24@gmail.com>
2023-07-12 11:51:52 +0200

    c

---
+++ frontend/src/components/shared/inputs/Inputs.jsx
@@ -0,0 +1,10 @@
+function Inputs() {
+  return (
+    <div className='input-container'>
+      <label>Escribe tu ciudad</label>
+      <input type='text'>Inputs</input>
+    </div>
+  );
+}
+
+export default Inputs;
