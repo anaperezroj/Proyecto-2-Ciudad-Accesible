a44625743459a8e40563b96e6dae79ae6ea1d926
Ana <e4a.perez24@gmail.com>
2023-07-13 12:57:33 +0200

    comentarios users

--- frontend/src/components/shared/block/Block.jsx
+++ frontend/src/components/shared/block/Block.jsx
@@ -1,12 +1,57 @@
-import "./block.css";
-
+import { useState, useEffect } from 'react';
+import './block.css';
+function getRandomTime() {
+  const randomHour = Math.floor(Math.random() * 24)
+    .toString()
+    .padStart(2, '0');
+  const randomMinute = Math.floor(Math.random() * 60)
+    .toString()
+    .padStart(2, '0');
+  return `${randomHour}:${randomMinute}`;
+}
 function Block() {
+  //Estado que almacena la url de la foto user
+  const [userPhoto, setUserPhoto] = useState('');
+  //Estado que almacena el nombre
+  const [userName, setUserName] = useState('');
+  //Estado para la hora
+  const [randomTime, setRandomTime] = useState('');
+
+  //useEffect para realizar solicitud a la api
+  useEffect(() => {
+    fetch('https://randomuser.me/api/')
+      .then((response) => response.json())
+      //Accedemos a la propiedad results[0] del obj data para obtener info user
+      .then((data) => {
+        const { picture, name } = data.results[0];
+        //Guardamos url de foto en estado
+        setUserPhoto(picture.large);
+        setUserName(`${name.first} ${name.last}`);
+        setRandomTime(getRandomTime());
+      })
+      .catch((error) => {
+        console.error('Error fetching user photo:', error);
+      });
+  }, []);
+
   return (
-    <div className="block-container">
-      <h2>título de la publicación</h2>
-      <div className="border-container">prueba Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum quia iste doloribus temporibus quam commodi dicta suscipit sunt corporis laudantium minus possimus, error veritatis, praesentium quibusdam illum quae assumenda amet!</div>
+    <div className='block-container'>
+      <div className='userPhoto'>
+        <img src={userPhoto} alt='user photo' />
+      </div>
+      <div className='userInfo'>
+        <h2>{userName}</h2>
+        <p>
+          Escribió a las
+          <span>{randomTime}</span>
+        </p>
+      </div>
+      <div className='border-container'>
+        ¡Esta web es realmente útil para nuestra ciudad! Gracias a ella ahora
+        podemos denunciar y visualizar lugares con problemas de accesibilidad en
+        nuestra comunidad.¡Excelente trabajo!
+      </div>
     </div>
   );
 }
-
 export default Block;
